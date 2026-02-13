de58eed3e0c55d21395086af89a66080017b3ecc
ohr25375 <kmm2559375@stu.o-hara.ac.jp>
2026-02-13 15:20:25 +0900

    Adjusted Bound Movement Handler
    Changed
    = Bound Movement Handler now clamps position properly

--- TheSkyCleaner/Assets/TheSkyCleaner/Codes/Scripts/Movement/BoundMovementHandler.cs
+++ TheSkyCleaner/Assets/TheSkyCleaner/Codes/Scripts/Movement/BoundMovementHandler.cs
@@ -20,7 +20,12 @@ public class BoundMovementHandler : MovementHandler
         {
             return base.ApplyTranslate(vector, space);
         }
-        return false;
+        Vector3 newPos = m_transform.position + vector ;
+        newPos.x = Mathf.Clamp(newPos.x, -m_bounds.extents.x + m_bounds.center.x, m_bounds.extents.x + m_bounds.center.x);
+        newPos.y = Mathf.Clamp(newPos.y, -m_bounds.extents.y + m_bounds.center.y, m_bounds.extents.y + m_bounds.center.y);
+        newPos.z = Mathf.Clamp(newPos.z, -m_bounds.extents.z + m_bounds.center.z, m_bounds.extents.z + m_bounds.center.z);
+        m_transform.position = newPos;
+        return true;
     }
 
     private bool IsInBounds(Vector3 to)
