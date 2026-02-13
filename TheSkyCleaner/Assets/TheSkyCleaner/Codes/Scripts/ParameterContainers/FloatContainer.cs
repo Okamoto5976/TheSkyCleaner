b332666013939db5f6a85f180114103049322119
syuu375 <kmm2559313@stu.o-hara.ac.jp>
2026-02-13 10:24:40 +0900

    ポーズ

--- TheSkyCleaner/Assets/TheSkyCleaner/Codes/Scripts/ParameterContainers/FloatContainer.cs
+++ TheSkyCleaner/Assets/TheSkyCleaner/Codes/Scripts/ParameterContainers/FloatContainer.cs
@@ -4,11 +4,10 @@ using UnityEngine.Events;
 [CreateAssetMenu(fileName = "FloatContainer", menuName = "Scriptable Objects/Parameter Containers/FloatContainer")]
 public class FloatContainer : RuntimeScriptableObject
 {
-    [SerializeField] private float m_initialValue=100;//‚P‚O‚Oƒ_ƒ‚â‚Á‚½‚ç‚¢‚Á‚Ä
+    [SerializeField] private float m_initialValue;
     [SerializeField] private float m_value;
 
     public float Value => m_value;
-    public float InitialValue => m_initialValue;//‚±‚ê‚àƒ_ˆÈ‰º—ª
     public void SetValue(float value)
     {
         m_value = value;
