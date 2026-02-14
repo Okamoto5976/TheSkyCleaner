8360e58532a368b0da52001d936e05c2383276e3
Okamoto5976 <kmm2559327@stu.o-hara.ac.jp>
2026-02-14 18:08:18 +0900

    Revert "so_package research"
    This reverts commit 4e2cdb4a3b74a0773afbb990006a8500b9a64ba7.

--- TheSkyCleaner/Assets/TheSkyCleaner/Codes/Scripts/Controllers/PlayerController.cs
+++ TheSkyCleaner/Assets/TheSkyCleaner/Codes/Scripts/Controllers/PlayerController.cs
@@ -7,11 +7,9 @@ public class PlayerController : MonoBehaviour
     [Header("Logger")]
     [SerializeField] private Logger m_logger;
 
-    [Header("Input")]
-    [SerializeField] private InputContainer m_inputContainer;
-
     [Header("Components")]
     [SerializeField] private TiltHandler m_playerTiltHandler;
+    [SerializeField] private ArmController m_armController;
     [SerializeField] private ReticleController m_reticleController;
     [SerializeField] private AnimatorVariableDriver m_animatorVariableDriver;
 
@@ -22,9 +20,13 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private PlayerStatus m_playerStatus;
 
     [Header("Events")]
+    [SerializeField] private InputContainer m_inputContainer;
+    [SerializeField] private UnityEvent<float> m_onChangeSpeed;
 
     private MovementHandler m_movementHandler;
     private PlayerAttackController m_playerAttackController;
+    private float m_strongHoldValue;
+    private float m_weakHoldValue;
     private Vector2 m_movementAxis;
     private Vector2 m_reticleAxis;
 
@@ -35,32 +37,34 @@ public class PlayerController : MonoBehaviour
         m_transform = transform;
         m_movementHandler = GetComponent<MovementHandler>();
         m_playerAttackController = GetComponent<PlayerAttackController>();
+        m_strongHoldValue = 0;
+        m_weakHoldValue = 0;
         m_movementAxis = Vector2.zero;
     }
 
     private void OnEnable()
     {
         m_inputContainer.StrongAction.Tap.OnTrigger += OnPlayerDodge;
-
+        m_inputContainer.StrongAction.HoldState.OnValueChanged += OnStrongHold;
+        m_inputContainer.WeakAction.HoldState.OnValueChanged += OnWeakHold;
     }
 
     private void OnDisable()
     {
         m_inputContainer.StrongAction.Tap.OnTrigger -= OnPlayerDodge;
-
+        m_inputContainer.StrongAction.HoldState.OnValueChanged -= OnStrongHold;
+        m_inputContainer.WeakAction.HoldState.OnValueChanged -= OnWeakHold;
     }
 
     private void Update()
     {
         m_playerStatus.UpdateMovementInput(m_inputContainer.MovementAxis);
-        PassReticle();
         MovePlayer(ref m_movementAxis);
         m_playerTiltHandler.TiltOnYaw(m_movementAxis);
         m_playerTiltHandler.TiltYaw(m_movementAxis.x);
-    }
+        ChangeSpeed(m_strongHoldValue + m_weakHoldValue);
 
-    private void FixedUpdate()
-    {
+        PassReticle();
     }
 
     private void PassReticle()
@@ -77,11 +81,21 @@ public class PlayerController : MonoBehaviour
     public void MovePlayer(ref Vector2 axis)
     {
         axis = m_inputContainer.MovementAxis;
-        m_movementHandler.SetSpeed(m_playerStatus.Speed);
         m_movementHandler.MoveOnZ(axis);
         m_playerStatus.UpdateGlobalPosition(m_transform.position);
         m_animatorVariableDriver.Drive(m_dodgeAnimationHorizontalFloatName.Value, axis.x);
     }
 
-
+    public void OnStrongHold(bool state)
+    {
+        m_strongHoldValue = state ? 1 : 0;
+    }
+    public void OnWeakHold(bool state)
+    {
+        m_weakHoldValue = state ? -1 : 0;
+    }
+    public void ChangeSpeed(float dir)
+    {
+        m_onChangeSpeed.Invoke(dir);
+    }
 }
