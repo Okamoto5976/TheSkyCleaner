a571c2a2d4fefd7981e90b7bc108c74302ac5517
ohr25375 <kmm2559375@stu.o-hara.ac.jp>
2026-03-11 14:09:32 +0900

    Humans now return to pool

--- TheSkyCleaner/Assets/TheSkyCleaner/Codes/Scripts/Human/HumanController.cs
+++ TheSkyCleaner/Assets/TheSkyCleaner/Codes/Scripts/Human/HumanController.cs
@@ -6,6 +6,7 @@ public class HumanController : MonoBehaviour, ILockOnTarget
     [SerializeField] private CollectSO m_collectSO;
     [SerializeField] private Vector3 m_moveDirection = Vector3.down;
     [SerializeField] private Vector3 m_reticleOffset = Vector3.zero;
+    [SerializeField] private float m_returnHeight = -10;
     public Vector3 ReticleOffset => m_reticleOffset;
     public Transform Transform => transform;
     public GameObject GameObject => gameObject;
@@ -22,6 +23,10 @@ public class HumanController : MonoBehaviour, ILockOnTarget
     private void Update()
     {
         m_movementHandler.MoveAllGlobal(m_moveDirection);
+        if (Transform.position.y < m_returnHeight)
+        {
+            ReturnToPool();
+        }
     }
 
     public void ReturnToPool()
